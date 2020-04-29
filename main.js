7f1a7371851b2defd50c2116e36bcd1d654e66d1
Zach Roark <zach.roark@yahoo.com>
2020-04-29 11:41:24 -0500

    project complete

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
+const array1 = [45, "I", true, null, "am", 3.56, "a", undefined, { catchphrase: "Oh hai, Mark"}, "JS", "rockstar"]
+const array2 = []
+
+for (let i=0; i < array1.length; i++) {
+    const item = array1[i];
+    if (typeof item === "string") {
+    array2.push(item)
+    }
+}
+
+console.log(array2);
+
+
+
 // Loop over every item in this array:
 
-[45, “I”, true, null, “am”, 3.56, “a”, undefined, { catchphrase: “Oh hai, Mark”}, “JS”, “rockstar”]
-
+// if typeof element is a string, then push it into array2
 // Return a new array that contains only strings. Log out the result.
 
 // // Hint: The typeof operator
