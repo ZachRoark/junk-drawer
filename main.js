916a9d14fa302fea7e4bddb6b8415d23386ea064
Zach Roark <zach.roark@yahoo.com>
2020-04-29 10:04:51 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// Loop over every item in this array:
+
+[45, “I”, true, null, “am”, 3.56, “a”, undefined, { catchphrase: “Oh hai, Mark”}, “JS”, “rockstar”]
+
+// Return a new array that contains only strings. Log out the result.
+
+// // Hint: The typeof operator
+// console.log(typeof 42);
+// // expected output: "number"
+
+// console.log(typeof 'blubber');
+// // expected output: "string"
+
+// console.log(typeof true);
+// // expected output: "boolean"
+
+// console.log(typeof undeclaredVariable);
+// // expected output: "undefined";
